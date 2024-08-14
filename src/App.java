c0df84c663e6f67bc8c012faa18fe70117ee962a
Alexius Maliuk Dias <alexiusmd2004@gmail.com>
2024-08-14 18:11:19 -0300

    Item 3

--- src/App.java
+++ src/App.java
@@ -1,13 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
-        Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45);
-        Carro utilitario = new Carro("Utilitario", TipoCombustivel.DIESEL, 5, 70);
+        Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55, TipoCombustivel.GASOLINA);
+        Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45, TipoCombustivel.GASOLINA);
+        Carro utilitario = new Carro("Utilitario", TipoCombustivel.DIESEL, 5, 70, TipoCombustivel.DIESEL);
+        Carro SUV = new Carro("SUV", TipoCombustivel.GASOLINA, 8, 55, TipoCombustivel.FLEX);
         System.out.println("Tipos de veiculos:");
         System.out.println(basico);
         System.out.println(esportivo);
         System.out.println(utilitario);
+        System.out.println(SUV);
 
         System.out.println("\n\n----------------");
         System.out.println("\nAbastencendo carro basico com gasolina");
@@ -35,5 +37,14 @@ public class App {
         utilitario.viaja(250);
         utilitario.viaja(150);
         System.out.println(utilitario);
+
+        System.out.println("\n\n----------------");
+        System.out.println("\nAbastencendo carro SUV com gasolina");
+        SUV.abastece(TipoCombustivel.GASOLINA, 55);
+        System.out.println(SUV);
+        System.out.println("\nViajando com o carro SUV");
+        SUV.viaja(250);
+        SUV.viaja(150);
+        System.out.println(SUV);
     }
 }
